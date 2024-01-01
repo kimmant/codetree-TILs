98ab480a0f5798ba56d6a6c75b433ff65abd9d53
kimmant <120030161+kimmant@users.noreply.github.com>
2024-01-01 21:53:48 +0900

    Title: 정수 선언하고 뺄셈, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240101/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 97;
+        int b = 13 ; 
+        System.out.print (a +" - "+ b +" = " + (a-b) );// 여기에 코드를 작성해주세요.
+    }
+}
