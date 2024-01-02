fb45904a7f23820fd048e7eef87c55fc5f8eadfa
kimmant <120030161+kimmant@users.noreply.github.com>
2024-01-02 14:48:37 +0900

    Title: 소수점 반올림하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240102/소수점 반올림하기/rounding-decimal-points.java
+++ 240102/소수점 반올림하기/rounding-decimal-points.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-       int a = 25.352;
+       double a = 25.352;
        System.ouut.printf("%1f",a); // 여기에 코드를 작성해주세요.
     }
 }
