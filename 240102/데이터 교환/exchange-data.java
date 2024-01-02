9f0e96c47e9ca9841d372239311ed4f3e87f886c
kimmant <120030161+kimmant@users.noreply.github.com>
2024-01-02 15:18:09 +0900

    Title: 데이터 교환, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

--- 240102/데이터 교환/exchange-data.java
+++ 240102/데이터 교환/exchange-data.java
@@ -1,13 +1,17 @@
 public class Main {
     public static void main(String[] args) {
       int a=5,b=6,c=7;
-      int temp1 , temp2;
+/*      int temp1 , temp2;
       temp1=a;
       temp2=b;
       a=c;
       b=temp1;
       c=temp2;
-
+*/
+       int temp = a;
+        a = c;
+        c = b;
+        b = temp;
       System.out.println(a);
       System.out.println(b);
       System.out.println(c);
