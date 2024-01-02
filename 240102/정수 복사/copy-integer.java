d39add9520e032aa89709cb7e8ce29ad7e5dd68f
kimmant <120030161+kimmant@users.noreply.github.com>
2024-01-02 15:10:46 +0900

    Title: 정수 복사, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240102/정수 복사/copy-integer.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+      int a =3 ;
+      int b =4;
+      b =a ;
+      System.out.println(a+" "+b);
+      System.out.println(a*b);  // 여기에 코드를 작성해주세요.
+    }
+}
