f4bcb7162f7367625010ba4ecf727606eacd9ea7
kimmant <120030161+kimmant@users.noreply.github.com>
2024-03-11 21:30:55 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240311/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int x,y;
+    cin.get();
+    cin.get();
+    cin.get();
+    cin.get();
+    cin>> x;
+    cin.get();
+    cin >> y;
+     cout<<"010"<<"-"<<y<<"-"<<x;
+ 
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
