32b86a7d29d954ebab3703e737827228d213a94a
kimmant <120030161+kimmant@users.noreply.github.com>
2024-03-11 22:00:22 +0900

    Title: 두수의 곱과 몫, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240311/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int a ,b ;
+    cin >> a >> b ; 
+    cout << a << " * "<< b << " = " << a*b <<endl;
+    cout << a << " / "<< b << " = " << (double)a/b <<endl;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
