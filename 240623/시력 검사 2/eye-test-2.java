7534ca074995fcf39327a963f752886826276e4a
kimmant <120030161+kimmant@users.noreply.github.com>
2024-06-23 17:24:21 +0900

    Title: 시력 검사 2, Time: 139ms, Memory: 11MB, Status: Passed - CodeTree

--- 240623/시력 검사 2/eye-test-2.java
+++ 240623/시력 검사 2/eye-test-2.java
@@ -8,7 +8,7 @@ public class Main {
         if (a >= 1.0) {
             System.out.println("High");
         }
-        else if (a >= 0.5 && a <1.0 ) {
+        else if (a >= 0.5 ) {
             System.out.println("Middle");
         }
 
