5b6ddb82ea68a12f19b466bd889d85392e6b2abe
kimmant <120030161+kimmant@users.noreply.github.com>
2024-06-26 17:39:24 +0900

    Title: 감소시키며 출력하기 2, Time: 81ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240626/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+          int i=26;
+
+        while (10 <= i) {
+          
+            System.out.print(i+" ");
+            
+            i--;
+        }
+        
+        
+    }
+}
