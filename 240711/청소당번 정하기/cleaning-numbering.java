38e05048520ee987d6a1ba2b5c26afdd4f639d91
kimmant <120030161+kimmant@users.noreply.github.com>
2024-07-11 20:02:33 +0900

    Title: 청소당번 정하기, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240711/청소당번 정하기/cleaning-numbering.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt1=0,cnt2=0,cnt3=0;
+        int n = sc.nextInt();
+
+        for (int i = 1; i <=n; i++) {
+            
+            if (i % 12 == 0) {
+                cnt3++;
+                
+            }
+            else if( i % 3 == 0){
+                cnt2++;
+            }
+            else if(i % 2 == 0){
+                cnt1++;
+            }
+        }
+
+        System.out.println(cnt1+" "+cnt2+" "+cnt3);
+    }
+}
